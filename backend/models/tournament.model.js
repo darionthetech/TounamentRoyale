f969f1bdba7eb4a04be87012cacf92dd20ec6566
Darion Curtis <darionthetech@gmail.com>
2021-11-30 04:24:28 -0600

    routes, Mongoose/ATLAS connection

--- backend/models/tournament.model.js
+++ backend/models/tournament.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-
+/* update schema with info from front end */
 const tournamentSchema = new Schema({
     username: { type: String, required: true },
     description: { type: String, required: true },
